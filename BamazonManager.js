0c348f88f828e43d65bcb1590b189d2e33146f3c
Karin Hermann <karinclimber@gmail.com>
2017-08-19 14:02:57 -0400

    almost working

--- BamazonManager.js
+++ BamazonManager.js
@@ -19,6 +19,27 @@ connection.connect(function(err) {
 });
 //start function 
 function start() {
+
+inquirer.prompt({
+	type: 'list',
+	name: 'toDo',
+	message: 'What would you like to do?',
+	choices: ["View Products for Sale", "View Low Inventory", "Add to Inventory", "Add New Product"]
+}).then(function(answers){
+	if (answers.toDo === "View Products for Sale") {
+		showTable();
+	} else if (answers.toDo === "View Low Inventory") {
+		lowInventory();
+	} else if (answers.toDo === "Add to Inventory") {
+				
+	} if (answers.toDo === "Add New Product") {
+						
+	} else {
+		var timer = setTimeout(start, 3000);
+	}
+})	
+//post table of data
+function showTable() {
 	connection.query("SELECT * FROM products", function(err, result, fields) {
 		if (err) throw err;
 		var data = result;
@@ -32,44 +53,58 @@ function start() {
 			t.newRow()
 		})
 		console.log(colors.rainbow(t.toString()));
-		inquirer.prompt({
-			type: 'input',
-			name: 'itemID',
-			message: 'What is the ID Number of the item that you would like to update?',
-			//grab product name based off of user input of ITEM ID     
-		}).then(function(answer) {
-			// console.log(answer.itemID);
-			connection.query("SELECT * FROM products WHERE ?", {
-				item_id: answer.itemID
-			}, function(err, res) {
-				if (err) throw (err);
-				console.log("You chose to update " + res[0].product_name + ".");
-				var chosenItem = answer.itemID;
-				inquirer.prompt({
-					type: 'input',
-					name: 'quantity',
-					message: 'How many would you like to add?',
-				}).then(function(answer) {
-					console.log(colors.blue("Great! You chose to add " + answer.quantity + "."));
-					confirm("Do you want to proceed with this update?").then(function confirmed() {
-						console.log(colors.green("Great! I have updated " + res[0].product_name + "!"));
-						// console.log(res[0].product_name);
-						var newStock = (parseInt(res[0].stock_quantity)) + (parseInt(answer.quantity));
-						// console.log(newStock);
-						connection.query("UPDATE products SET ? WHERE ?", [{
-							stock_quantity: newStock
-						}, {
-							product_name: res[0].product_name
-						}], function(err, res) {
-							var timer2 = setTimeout(start, 2000)
-							if (err) throw err;
-						})
-					}, function cancelled() {
-						console.log("Okay, let me know when you are ready!")
-						var timer2 = setTimeout(start, 2000)
-					})
-				})
-			})
-		})
 	})
+};
+
+function lowInventory() {
+	connection.query("SELECT * FROM products", function(err,res) {
+		if (err) throw err;
+		for (var i = 0; i < res.length ; i ++) {
+				if (res[i].stock_quantity <= 5) {
+			console.log([res[i].product_name, res[i].stock_quantity]);
+			
+		} else {
+			console.log("boogers")
+		}
+	}
+	})
+}
+	
+	// connection.query("SELECT * FROM products WHERE ?", {stock_quantity: 1}, function(err, res) {
+	// 	var newArr = arr.push(res);
+	// console.log(newArr);
+	// })
+	
+	
+
+	// connection.query("SELECT * FROM products WHERE ?", {
+	// 		}, function(err, res) {
+	// 			if (err) throw (err);
+	// 			console.log("You chose to update " + res[0].product_name + ".");
+	// 			var chosenItem = answer.itemID;
+	// 			inquirer.prompt({
+	// 				type: 'input',
+	// 				name: 'quantity',
+	// 				message: 'How many would you like to add?',
+	// 			}).then(function(answer) {
+	// 				console.log(colors.blue("Great! You chose to add " + answer.quantity + "."));
+	// 				confirm("Do you want to proceed with this update?").then(function confirmed() {
+	// 					console.log(colors.green("Great! I have updated " + res[0].product_name + "!"));
+	// 					// console.log(res[0].product_name);
+	// 					var newStock = (parseInt(res[0].stock_quantity)) + (parseInt(answer.quantity));
+	// 					// console.log(newStock);
+	// 					connection.query("UPDATE products SET ? WHERE ?", [{
+	// 						stock_quantity: newStock
+	// 					}, {
+	// 						product_name: res[0].product_name
+	// 					}], function(err, res) {
+	// 						var timer2 = setTimeout(start, 2000)
+	// 						if (err) throw err;
+	// 					})
+	// 				}, function cancelled() {
+	// 					console.log("Okay, let me know when you are ready!")
+	// 					var timer2 = setTimeout(start, 2000)
+				// 	})
+				// })
+			// })
 }
